aee332070b5deb4bca63e9bd9ecc346eaa4e514b
BoMaassen <bomaassen@live.nl>
2025-01-16 09:57:05 +0100

    Initial commit

---
+++ src/main/java/nl/bo/bacpatternpoint/BacPatternPointApplication.java
@@ -0,0 +1,13 @@
+package nl.bo.bacpatternpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacPatternPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BacPatternPointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/bo/bacpatternpoint/BacPatternPointApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.bo.bacpatternpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacPatternPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
